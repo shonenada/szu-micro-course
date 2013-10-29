8e4764fd5da61b8d41e34b71382faed0f0a58eaf
shonenada <shonenada@gmail.com>
2013-10-29 19:49:52 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+install_required = [l.strip() for l in open("requirements.txt", "r")]
+
+
+metadata = {'name': 'szu-mooc',
+            'version': '0.1',
+            'packages': find_packages(),
+            'author': 'shonenada @ League of Tech Team',
+            'author_email': 'shonenada@gmail.com',
+            'url': "https://github.com/league-of-tech/mooc",
+            'zip_safe': False,
+            'platforms': ['linux'],
+            'package_data': {"": ['*.html', '*.jpg', '*.png', '*.css', '*.js',
+                                  '*.ico', '*.coffee', '*.less', '*.stylus']},
+            'install_required': install_required,
+            'description': 'A MOOC system for SZU.'}
+
+
+if __name__ == '__main__':
+    setup(**metadata)
