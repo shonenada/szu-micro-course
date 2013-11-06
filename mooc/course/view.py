2638f214d9b6e5f50b6381db8a4ef823bcaa6c33
shonenada <shonenada@gmail.com>
2013-11-07 00:57:16 +0800

    add subject view.

--- mooc/course/view.py
+++ mooc/course/view.py
@@ -9,6 +9,11 @@ def courses():
     return render_template('courses.html')
 
 
-@course_app.route('/courses/category/<id>')
-def category(id):
+@course_app.route('/courses/subject/<sid>')
+def subject(sid):
+    return render_template('subject.html')
+
+
+@course_app.route('/courses/subject/<sid>/category/<cid>')
+def category(sid, cid):
     return render_template('category.html');
