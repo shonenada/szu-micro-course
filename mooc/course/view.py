a4450d23c044079fac83c29e9904a1a85eda047f
shonenada <shonenada@gmail.com>
2013-11-06 21:28:54 +0800

    Added view file.

--- mooc/course/view.py
+++ mooc/course/view.py
@@ -6,4 +6,9 @@ course_app = Blueprint('course', __name__, template_folder='../templates')
 
 @course_app.route('/courses')
 def courses():
-    return render_template("course_list.html")
+    return render_template('courses.html')
+
+
+@course_app.route('/courses/category/<id>')
+def category(id):
+    return render_template('category.html');
