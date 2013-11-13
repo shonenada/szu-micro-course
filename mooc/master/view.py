674520416781bb53e0245249fb7a7a2689f38444
shonenada <shonenada@gmail.com>
2013-11-13 17:31:17 +0800

    Fill data in index.html

--- mooc/master/view.py
+++ mooc/master/view.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 
+from mooc.course.model import Subject
+
 
 master_app = Blueprint('master', __name__, template_folder='../templates')
 
 
 @master_app.route('/')
 def index():
-    return render_template("index.html")
+    subjects = Subject.query.all()
+    return render_template("index.html", subjects=subjects)
