0aa68ab1cb515137625514524e2e466f9189d97f
shonenada <shonenada@gmail.com>
2013-11-13 21:22:51 +0800

    Move friendly_date from mooc/template.py to mooc/utils.py, and add new filter.

--- mooc/course/service.py
+++ mooc/course/service.py
@@ -10,3 +10,10 @@ def get_learn_records():
 def get_last_clip():
     app.jinja_env.globals['last_clip'] = \
         Clip.query.order_by(Clip.upload_time.desc()).limit(10).all()
+
+
+def learn_count(course):
+    learn_count = 0
+    for c in course.clip:
+        learn_count = learn_count + c.learn_record.count()
+    return learn_count
