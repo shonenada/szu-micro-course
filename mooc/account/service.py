012b1b7d9c713a4e8ee3d888cb5ee94223a03d06
shonenada <shonenada@gmail.com>
2013-11-15 18:07:10 +0800

    Added services of account module, and new handlers

--- mooc/account/service.py
+++ mooc/account/service.py
@@ -1,5 +1,19 @@
-from mooc.account.model import SzuAccount
+from time import time
+
+from flask import g, session
+
+from mooc.account.model import User, SzuAccount
 
 
-def do_signin(cardid, password):
-    pass
+def login(user, expires=129600):
+    if type(user) is User:
+        g.current_user = user
+        expires += time()
+        session['uid'] = (user.id, expires)
+        return user
+    return None
+
+
+def logout():
+    del session['uid']
+    g.current_user = None
--- mooc/account/view.py
+++ mooc/account/view.py
@@ -1,17 +1,27 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 
-from mooc.account.service import do_signin
+from mooc.account.model import User
+from mooc.account.service import login
 
 
 account_app = Blueprint('account', __name__, template_folder='../templates')
 
 
-@account_app.route('/signin', methods=['GET', 'POST'])
+@account_app.route('/signin', methods=['GET'])
 def signin():
-    if request.method == 'GET':
-        return render_template('account/sign-in.html')
-    elif request.method == 'POST':
-        do_signin()
+    return render_template('account/sign-in.html')
+
+
+@account_app.route('/signin', methods=['POST'])
+def do_signin():
+    username = request.form['username']
+    raw_passwd = request.form['password']
+    user = User.query.authenticate(username, raw_passwd)
+    if user:
+        login(user)
+        return redirect(url_for('master.index'))
+    else:
+        return 'Wrong'
 
 
 @account_app.route('/forgot')
