bd5bfb51b300e5777ffaf168879ef2869af22790
shonenada <shonenada@gmail.com>
2013-11-17 19:52:38 +0800

    Added extension

--- mooc/app.py
+++ mooc/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from mooc.extensions import gears, setup_compilers, setup_compressors
 from mooc.extensions import db, setup_database
+from mooc.extensions import login_manager
 from mooc.template import setup_filter
 from mooc.master.view import master_app
 from mooc.account.view import account_app
@@ -25,6 +26,8 @@ def create_app(import_name=None, config=None):
 
     db.init_app(app)
     setup_database(app)
+
+    login_manager.init_app(app)
 
     app.register_blueprint(master_app)
     app.register_blueprint(account_app)
