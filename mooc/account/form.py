726acd1bdf8e0ef31a623aa6994a3a4a8be9818d
shonenada <shonenada@gmail.com>
2013-11-20 02:22:04 +0800

    Update form.

--- mooc/account/form.py
+++ mooc/account/form.py
@@ -1,13 +1,13 @@
 #-*-coding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import TextField, BooleanField, PasswordField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired
 
 
 class SignInForm(Form):
     username = TextField(
         u'用户名',
-        validators=[InputRequired(message=u'用户名不能为空'), Email(message=u'邮箱')])
+        validators=[InputRequired(message=u'用户名不能为空')])
     password = PasswordField(
         u'密码',
         validators=[InputRequired(message=u'密码不能为空')])
