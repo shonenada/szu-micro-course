4e520010fb6ea61598c076b217e712989f56e2dd
shonenada <shonenada@gmail.com>
2013-11-24 01:21:51 +0800

    Added base template of back-end

--- mooc/master/view.py
+++ mooc/master/view.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 
+from mooc.app import rbac
 from mooc.course.model import Subject
 
 
@@ -7,6 +8,13 @@ master_app = Blueprint('master', __name__, template_folder='../templates')
 
 
 @master_app.route('/')
+@rbac.allow(['everyone'], ['GET'])
 def index():
     subjects = Subject.query.all()
-    return render_template("index.html", subjects=subjects)
+    return render_template('index.html', subjects=subjects)
+
+
+@master_app.route('/master')
+@rbac.allow(['super_admin'], ['GET'])
+def master_index():
+    return render_template('admin/index.html')
