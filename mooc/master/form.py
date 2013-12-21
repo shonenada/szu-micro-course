e0f9a88790695015aa81e07fb8fa793ec9be42a2
shonenada <shonenada@gmail.com>
2013-12-21 21:23:40 +0800

    add TagsField .

---
+++ mooc/master/form.py
@@ -0,0 +1,41 @@
+from wtforms.fields import Field
+from wtforms.widgets import TextInput
+
+
+class TagsField(Field):
+    widget = TextInput()
+
+    def __init__(self, tag_model, label='', validators=None, sep=u' ', **kwargs):
+        super(TagsField, self).__init__(label, validators, **kwargs)
+        self.tag_model = tag_model
+        self.sep = sep
+
+    def _value(self):
+        if self.data:
+            return self.sep.join([tag.tag for tag in self.data])
+        else:
+            return u''
+            
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = []
+            form_data = valuelist[0].split(self.sep)
+            for tag in form_data:
+                if not tag.strip():
+                    continue
+                q_tag = self.tag_model.query.filter_by(tag=tag).first()
+                if not q_tag:
+                    q_tag = self.tag_model(tag)
+                self.data.append(q_tag)
+        else:
+            self.data = []
+            self.data = list(self._remove_duplicates(self.data))
+
+    @classmethod
+    def _remove_duplicates(cls, seq):
+        """Remove duplicates in a case insensitive, but case preserving manner"""
+        d = {}
+        for item in seq:
+            if item.lower() not in d:
+                d[item.lower()] = True
+                yield item
