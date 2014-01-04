d1434c7ff382a68f3318b13575d410060e4d2d05
shonenada <shonenada@gmail.com>
2014-01-04 20:23:20 +0800

    update resource

--- mooc/resource/view.py
+++ mooc/resource/view.py
@@ -13,5 +13,8 @@ def list():
     resource_query = (Resource.query.filter(Resource.state != 'DELETED')
                               .order_by(Resource.created.desc()))
     resources = resource_query.paginate(page_num, per_page=20)
-    hot_resources = Resource.query.order_by(Resource.view_count.desc()).limit(30).all()
-    return render_template('resource/list.html', pagination=resources, hot_resources=hot_resources)
+    hot_resources = Resource.query.order_by(Resource.view_count
+                                                    .desc()).limit(30).all()
+    return render_template('resource/list.html',
+                           pagination=resources,
+                           hot_resources=hot_resources)
