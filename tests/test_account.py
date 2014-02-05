c24bb153ecfe36f4560961c4d5192447bb922f57
shonenada <shonenada@gmail.com>
2014-02-05 17:58:46 +0800

    update test_account.py

--- tests/test_account.py
+++ tests/test_account.py
@@ -13,14 +13,16 @@ class SigninUnitTests(BaseSuite):
     def test_required_fields(self):
         rv = self.client.post('/account/signin')
         data = rv.data
-        self.assertIn('flash-message-box', data)
+        self.assertIn('"success": false', data)
+        self.assertIn('"errors": true', data)
         self.assertIn('This field is required', data)
 
     def test_success(self):
         rv = self.client.post('/account/signin', data={
             'username': 'normal_user', 'password': 'passwd'
         })
-        self.assertEqual(rv.status_code, 302)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn('"success": true', rv.data)
         rv = self.client.post('/account/signin', data={
             'username': 'staff', 'password': 'passwd'
         }, follow_redirects=True)
@@ -30,8 +32,7 @@ class SigninUnitTests(BaseSuite):
         rv = self.client.post('/account/signin', data={
             'username': 'no_this_user', 'password': 'wrong_password'
         }, follow_redirects=True)
-        self.assertIn('flash-message-box', rv.data)
-        self.assertIn('<body class="">', rv.data)
+        self.assertIn('"success": false', rv.data)
 
     def prehook(self):
         self.prepare_role()
