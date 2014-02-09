ffca767044252040db638b1c510726d965ffacef
shonenada <shonenada@gmail.com>
2014-02-09 17:49:01 +0800

    download count instead of view count

--- mooc/views/resource.py
+++ mooc/views/resource.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
 from mooc.extensions import db, rbac
 from mooc.models.resource import Resource
@@ -18,3 +18,12 @@ def list():
     return render_template('resource/list.html',
                            pagination=resources,
                            hot_resources=hot_resources)
+
+
+@resource_app.route('/download/<int:rid>')
+@rbac.allow(['local_user'], ['GET'])
+def download(rid):
+    res = Resource.query.get_or_404(rid)
+    res.download_count = res.download_count + 1
+    res.save()
+    return redirect(res.resource_url)
